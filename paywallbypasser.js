062f6904e16cdb1badf249eea2a6ebc870f7bb4a
Leo Malinen <107366630+leomalinen04@users.noreply.github.com>
2024-08-08 17:05:12 -0700

    First iteration of the paywallbypasser

---
+++ paywallbypasser.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Paywall Bypasser
+// @version      1.0
+// @description  Allows users to bypass the paywall of (almost) any article they want with a few other options.
+// @author       LeoMalinen
+// @license      NONE
+// @include      *://*
+// @icon         none
+// @grant        GM_registerMenuCommand
+// @grant        GM_openInTab
+// ==/UserScript==
+
+GM_registerMenuCommand("Replace the Current Tab", function() {
+    var currentUrl = window.location.href;
+    var proxyUrl = 'https://12ft.io/proxy?q=' + encodeURIComponent(currentUrl);
+    window.location.href = proxyUrl;
+}, "u");
+
+GM_registerMenuCommand("Open in a New Tab", function() {
+    var currentUrl = window.location.href;
+    var proxyUrl = 'https://12ft.io/proxy?q=' + encodeURIComponent(currentUrl);
+    GM_openInTab(proxyUrl, { active: true });
+});
+GM_registerMenuCommand("Open in an Incognito Tab", function() {
+    console.log("Opening in Incognito Tab");
+    var currentUrl = window.location.href;
+    var proxyUrl = 'https://12ft.io/proxy?q=' + encodeURIComponent(currentUrl);
+    GM_openInTab(proxyUrl, { active: true, incognito: true });
+});
