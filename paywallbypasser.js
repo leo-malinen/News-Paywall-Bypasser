b495e11b7bd54ab9337dde480c09cca6c066ccc7
Leo Malinen <107366630+leomalinen04@users.noreply.github.com>
2024-09-26 14:09:20 -0700

    Update paywallbypasser.js to change from 12ft.io to pbc

--- paywallbypasser.js
+++ paywallbypasser.js
@@ -12,13 +12,13 @@
 
 GM_registerMenuCommand("Replace the Current Tab", function() {
     var currentUrl = window.location.href;
-    var proxyUrl = 'https://12ft.io/proxy?q=' + encodeURIComponent(currentUrl);
+    var proxyUrl = 'https://bypasspaywallsclean.com/proxy?q=' + encodeURIComponent(currentUrl);
     window.location.href = proxyUrl;
 }, "u");
 
 GM_registerMenuCommand("Open in a New Tab", function() {
     var currentUrl = window.location.href;
-    var proxyUrl = 'https://12ft.io/proxy?q=' + encodeURIComponent(currentUrl);
+    var proxyUrl = 'https://bypasspaywallsclean.com/proxy?q=' + encodeURIComponent(currentUrl);
     if (!window.openedTab) {
         window.openedTab = GM_openInTab(proxyUrl, { active: true });
     }
@@ -27,9 +27,8 @@ GM_registerMenuCommand("Open in a New Tab", function() {
 GM_registerMenuCommand("Open in an Incognito Tab", function() {
     console.log("Opening in Incognito Tab");
     var currentUrl = window.location.href;
-    var proxyUrl = 'https://12ft.io/proxy?q=' + encodeURIComponent(currentUrl);
+    var proxyUrl = 'https://bypasspaywallsclean.com/proxy?q=' + encodeURIComponent(currentUrl);
     if (!window.openedIncognitoTab) {
         window.openedIncognitoTab = GM_openInTab(proxyUrl, { active: true, incognito: true });
     }
 });
-
