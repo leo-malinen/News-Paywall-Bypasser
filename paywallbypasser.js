d0cda6b87e810de04808f247e1efc782c59f567e
Leo Malinen <107366630+leomalinen04@users.noreply.github.com>
2024-09-26 14:16:35 -0700

    Update paywallbypasser.js change from 'q' or query to url for it to work

--- paywallbypasser.js
+++ paywallbypasser.js
@@ -12,13 +12,13 @@
 
 GM_registerMenuCommand("Replace the Current Tab", function() {
     var currentUrl = window.location.href;
-    var proxyUrl = 'https://smry.ai/proxy?q=' + encodeURIComponent(currentUrl);
+    var proxyUrl = 'https://smry.ai/proxy?url=' + encodeURIComponent(currentUrl);
     window.location.href = proxyUrl;
 }, "u");
 
 GM_registerMenuCommand("Open in a New Tab", function() {
     var currentUrl = window.location.href;
-    var proxyUrl = 'https://smry.ai/proxy?q=' + encodeURIComponent(currentUrl);
+    var proxyUrl = 'https://smry.ai/proxy?url=' + encodeURIComponent(currentUrl);
     if (!window.openedTab) {
         window.openedTab = GM_openInTab(proxyUrl, { active: true });
     }
@@ -27,7 +27,7 @@ GM_registerMenuCommand("Open in a New Tab", function() {
 GM_registerMenuCommand("Open in an Incognito Tab", function() {
     console.log("Opening in Incognito Tab");
     var currentUrl = window.location.href;
-    var proxyUrl = 'https://smry.ai/proxy?q=' + encodeURIComponent(currentUrl);
+    var proxyUrl = 'https://smry.ai/proxy?url=' + encodeURIComponent(currentUrl);
     if (!window.openedIncognitoTab) {
         window.openedIncognitoTab = GM_openInTab(proxyUrl, { active: true, incognito: true });
     }
